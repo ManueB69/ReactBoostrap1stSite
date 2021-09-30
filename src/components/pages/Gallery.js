bd113448ff37bb4ae31df1e51bf9ab0c4b147d86
ManueB69 <e.bernardi69@gmail.com>
2021-09-30 14:29:16 +0200

    Added an API load to display gallery items

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,14 +1,53 @@
-const Gallery = ()=> (
+import { useEffect, useState } from "react";
+
+const Gallery = ()=> {
+  
+  // je déclare une variable gallery qui se remplit via la fonction setGallery, et je l'initialise comme étant un tableau vide
+  const [gallery, setGallery] = useState([])
+  
+  const loadGallery = ()=> {
+    fetch("https://picsum.photos/v2/list?page=2&limit=6")
+    .then(response => response.json() )
+    .then(data => {
+      console.log(data);
+      setGallery(data);
+    })
+  }
+  
+  useEffect( ()=>{
+    loadGallery();
+  }, [])
+  
+  const ListImages = ()=> (
+    <div className="row">
+    {gallery.map( (img) => {
+      const url="https://picsum.photos/id/" + img.id + "/600/400/";
+      return (
+      // syst requiert que les enfants d'1 liste aient une key unique
+        <div key={ img.id } className="col-12 col-md-4 mb-3">
+          <img src={ url } className="img-fluid" alt={ "from" + img.author } />
+          <h4 className="text-center">From { img.author } </h4>
+        </div>
+      )
+    })
+    }
+    </div>)
+  
+  return (
     <section>
         <div className="container-fluid">
           <div className="row">
             <h3>Gallery</h3>
           </div>
+          
           <div className="row">
-            <p>Quam ob rem circumspecta cautela observatum est deinceps et cum edita montium petere coeperint grassatores, loci iniquitati milites cedunt. ubi autem in planitie potuerint reperiri, quod contingit adsidue, nec exsertare lacertos nec crispare permissi tela, quae vehunt bina vel terna, pecudum ritu inertium trucidantur..</p>
-          </div>
+            <p>Quam ob rem circumspecta cautela observatum est deinceps et cum edita montium petere coeperint grassatores, loci iniquitati milites cedunt.</p>
+          </div>    
+          
+          <ListImages /> 
         </div>
       </section>
-)
+  )
+}
 
 export default Gallery
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,42 +7,42 @@ const Home = ()=> (
           
           <div className="row">
             <div className="col-12 col-md-6">
-              <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                   <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                   <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 </div>
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img src={require('../../assets/IMG_1.jpg').default} class="d-block w-100" alt="photo Cancun" />
-                    <div class="carousel-caption d-none d-sm-block">
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img src={require('../../assets/IMG_1.jpg').default} className="d-block w-100" alt="Cancun" />
+                    <div className="carousel-caption d-none d-sm-block">
                       <h5>Cancun</h5>
                       <p>Notre première plage caribéenne.</p>
                     </div>
                   </div>
-                  <div class="carousel-item">
-                    <img src={require('../../assets/IMG_2.jpg').default} class="d-block w-100" alt="photo plage" />
-                    <div class="carousel-caption d-none d-sm-block">
+                  <div className="carousel-item">
+                    <img src={require('../../assets/IMG_2.jpg').default} className="d-block w-100" alt="plage" />
+                    <div className="carousel-caption d-none d-sm-block">
                       <h5>Lever de soleil au sud de Cancun</h5>
                       <p>Les oiseaux marins forment un ballet de pêche</p>
                     </div>
                   </div>
-                  <div class="carousel-item">
-                    <img src={require('../../assets/IMG_3.jpg').default} class="d-block w-100" alt="photo plage" /> 
-                    <div class="carousel-caption d-none d-sm-block">
+                  <div className="carousel-item">
+                    <img src={require('../../assets/IMG_3.jpg').default} className="d-block w-100" alt="plage" /> 
+                    <div className="carousel-caption d-none d-sm-block">
                       <h5>Cozumel</h5>
                       <p>Magnifique... mais infesté de moustiques dès le soleil couché !!</p>
                     </div>
                   </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Précédent</span>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Précédent</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Suivant</span>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Suivant</span>
                 </button>
               </div>
             </div>
