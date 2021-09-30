1f3289bea424b58859f8a46b19fffd7630edde6f
ManueB69 <e.bernardi69@gmail.com>
2021-09-30 09:59:40 +0200

    Cleared App folder, tested Bootstrap and Structured the page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
@@ -6,20 +5,24 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Header">
+        <h1>Header</h1>
       </header>
+      
+      <section>
+        <div className="container-fluid">
+          <div className="row">
+            <h3>Home</h3>
+          </div>
+          <div className="row">
+            <p>Ob haec et huius modi multa, quae cernebantur in paucis, omnibus timeri sunt coepta. et ne tot malis dissimulatis paulatimque serpentibus acervi crescerent aerumnarum, nobilitatis decreto legati mittuntur: Praetextatus ex urbi praefecto et ex vicario Venustus et ex consulari Minervius oraturi, ne delictis supplicia sint grandiora, neve senator quisquam inusitato et inlicito more tormentis exponeretur.</p>
+          </div>
+        </div>
+      </section>
+      
+      <footer>
+        <h2>Footer</h2>
+      </footer>
     </div>
   );
 }
